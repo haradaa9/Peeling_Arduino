90dbf92e3738507a5958c1f4c6048cc8d99811c6
atsushi harada <atsushi9.harada@lixil.com>
2018-07-13 22:50:58 +0900

    add LED func & drill func

--- Peeling_Arduino.ino
+++ Peeling_Arduino.ino
@@ -1,16 +1,42 @@
 #include <VarSpeedServo.h>
 
-//rotation servo
+// rotation servo instance
 VarSpeedServo rotateServo;
-
-//seasow servo
+// seasow servo instance
 VarSpeedServo seasowServo;
 
+// const arduino pin
+const int redPin = 10, greenPin = 11, bluePin = 12;
+const int seasowPin = 8, rotatePin = 9;
+const int drillPin = 7;
 
-int rotatePos = 0;
+// difinite variable
 
-void seasowCtl(int rotatePos) {
-  //control seasow with seasowServo
+/*------------color------------*/
+void RGB(int red, int green, int blue){
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
+
+void LEDCtl(char color){
+  switch(color){
+    case 'b':
+      RGB(0, 255, 255);
+      break;
+    case 'r':
+      RGB(255, 0, 255);
+      break;
+    case 'g':
+      RGB(255, 255, 0);
+      break;
+  }
+}
+
+
+/*------------servo------------*/
+void seasowCtl(int rotatePos){
+  // control seasow with seasowServo
   switch(rotatePos){
     case 50:
       seasowServo.write(68, 15, true);
@@ -37,21 +63,40 @@ void seasowCtl(int rotatePos) {
   seasowServo.write(90, 15, true);
 }
 
+/*------------main------------*/
 void setup() {
   // put your setup code here, to run once:
-  //servo init
-  rotateServo.attach(9, 530, 2330);
+  // servo init
+  rotateServo.attach(rotatePin, 530, 2330);
   rotateServo.write(90);
-  seasowServo.attach(8, 570, 2370);
+  seasowServo.attach(seasowPin, 570, 2370);
   seasowServo.write(90);
+  // LED init
+  LEDCtl('r');
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(rotatePos = 180; rotatePos >= 50; rotatePos = rotatePos -10){
+  // start func
+  digitalWrite(drillPin, HIGH);
+  LEDCtl('g');
+
+  // Servo func
+  for(int rotatePos = 180; rotatePos >= 50; rotatePos = rotatePos - 10){
+    LEDCtl('g');
     rotateServo.write(rotatePos, 15, true);
+    LEDCtl('b');
     seasowCtl(rotatePos);
+    LEDCtl('g');
   }
+
+  // finish func
+  rotateServo.write(90, 15, true);
+  digitalWrite(drillPin, LOW);
+  LEDCtl('r');
   while(1);
 }
+
+
+
 
